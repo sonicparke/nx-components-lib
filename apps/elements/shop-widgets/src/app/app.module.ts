cb04f2fcf52fb0b6ded13a8f25132c51f503b050
Brad McAlister <brad.mcalister@vitals.com>
2019-09-25 15:56:04 -0500

    hooking up lib component to elements app

--- apps/elements/shop-widgets/src/app/app.module.ts
+++ apps/elements/shop-widgets/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddToCartComponent } from '@nx-components-lib/shop-widgets/components';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AddToCartComponent
+  ],
+  entryComponents: [AddToCartComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor (private injector: Injector) {} ngDoBootstrap() {
+    const elAddToCartComponent = createCustomElement(AddToCartComponent, { injector: this.injector });
+    customElements.define('web-components-root', elAddToCartComponent);
+    }
+}
