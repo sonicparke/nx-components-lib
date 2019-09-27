1cfb9050ad650d12d9645e85a9f733688a9364cd
Brad McAlister <brad.mcalister@vitals.com>
2019-09-27 11:42:16 -0500

    add lib exports

--- libs/shop-widgets/src/index.ts
+++ libs/shop-widgets/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/shop-widgets.module';
+export * from './lib/cart/cart.module';
+export * from './lib/add-to-cart/add-to-cart.module';
+export * from './lib/add-to-cart/add-to-cart.component';
+export * from './lib/cart/cart.component';
