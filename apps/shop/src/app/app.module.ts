8b81d8c99c4b1f06ffbdaa83ea03919812cbb51d
Brad McAlister <brad.mcalister@vitals.com>
2019-09-27 14:41:46 -0500

    adding angular material

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShopWidgetsModule } from '@nx-components-lib/shop-widgets';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ShopWidgetsModule
+    ShopWidgetsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- libs/shop-widgets/src/lib/add-to-cart/add-to-cart.module.ts
+++ libs/shop-widgets/src/lib/add-to-cart/add-to-cart.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddToCartComponent } from './add-to-cart.component';
-
+import { MatButtonModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [AddToCartComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [AddToCartComponent]
 })
