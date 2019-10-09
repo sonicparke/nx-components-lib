58ce48dad6b7fde99c05dae9e43293ff1fb5e2b6
Juri <juri.strumpflohner@gmail.com>
2019-10-09 21:50:09 +0200

    fix static elements example
    So the main issue is that you were
    referencing the components, basically
    redeclaring them in the elements app.
    It's better to import the modules directly 🙂

--- apps/elements/shop-widgets/src/app/app.module.ts
+++ apps/elements/shop-widgets/src/app/app.module.ts
@@ -1,27 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
-import { AddToCartComponent, CartComponent } from '@nx-components-lib/shop-widgets';
+import {
+  AddToCartComponent,
+  CartComponent,
+  AddToCartModule,
+  CartModule
+} from '@nx-components-lib/shop-widgets';
 import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AddToCartComponent,
-    CartComponent
+    // AddToCartComponent,
+    // CartComponent
   ],
   entryComponents: [
-    AddToCartComponent,
-    CartComponent
+    // AddToCartComponent,
+    // CartComponent
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AddToCartModule, CartModule],
   providers: [],
   bootstrap: []
 })
 export class AppModule {
-  constructor (private injector: Injector) {} ngDoBootstrap() {
-    const elAddToCartComponent = createCustomElement(AddToCartComponent, { injector: this.injector });
-    customElements.define('nx-components-lib-add-to-cart', elAddToCartComponent);
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const elAddToCartComponent = createCustomElement(AddToCartComponent, {
+      injector: this.injector
+    });
+    customElements.define(
+      'nx-components-lib-add-to-cart',
+      elAddToCartComponent
+    );
 
-    const elCartComponent = createCustomElement(CartComponent, { injector: this.injector });
+    const elCartComponent = createCustomElement(CartComponent, {
+      injector: this.injector
+    });
     customElements.define('nx-components-lib-cart', elCartComponent);
   }
 }
